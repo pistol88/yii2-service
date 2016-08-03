d36efdda4138a1781d4a6b3c5d1adbf8b3cc84f0
pistol88 <pistol88@gmail.com>
2016-08-03 17:01:36 +0500

    fix smena

--- views/session/current.php
+++ views/session/current.php
@@ -4,6 +4,7 @@ use yii\helpers\Html;
 use yii\helpers\Url;
 use pistol88\worksess\widgets\ControlButton;
 use pistol88\worksess\widgets\Info;
+use pistol88\worksess\widgets\SessionGraph;
 
 $this->title = 'Рабочая смена';
 $this->params['breadcrumbs'][] = $this->title;
@@ -15,39 +16,26 @@ $this->params['breadcrumbs'][] = $this->title;
         <?=$this->render('../parts/menu');?>
     </div>
     
-    <?php if(Yii::$app->session->hasFlash('success')) { ?>
-        <div class="alert alert-success" role="alert">
-            <?= Yii::$app->session->getFlash('success') ?>
-        </div>
-    <?php } ?>
-    <?php if(Yii::$app->session->hasFlash('fail')) { ?>
-        <div class="alert alert-danger" role="alert">
-            <?= Yii::$app->session->getFlash('fail') ?>
-        </div>
-    <?php } ?>
+    
+        <?php if(Yii::$app->session->hasFlash('success')) { ?>
+            <div class="alert alert-success" role="alert">
+                <?= Yii::$app->session->getFlash('success') ?>
+            </div>
+        <?php } ?>
+        <?php if(Yii::$app->session->hasFlash('fail')) { ?>
+            <div class="alert alert-danger" role="alert">
+                <?= Yii::$app->session->getFlash('fail') ?>
+            </div>
+        <?php } ?>
+    
     
     <h2>Смена</h2>
-    <?=Info::widget();?>
+    <div class="administrator-place">
+        <?=Info::widget();?>
 
-    <p><?=ControlButton::widget();?></p>
+        <?=ControlButton::widget();?>
+    </div>
     
-    <h2>Сотрудники</h2>
-    
-    <ul class="session-workers">
-        <?php foreach($workers as $worker) { ?>
-            <li class="row">
-                <div class="col-md-3">
-                    <?php if($worker->image) { ?>
-                        <img src="<?=$worker->image->getUrl('100x100');?>" />
-                    <?php } ?>
-                </div>
-                <div class="col-md-4">
-                    <p><strong><?=$worker->name;?></strong></p>
-                    <?=Info::widget(['for' => $worker]);?>
-                </div>
-                <div class="col-md-2"><div class="big-button"><?=ControlButton::widget(['for' => $worker]);?></div></div>
-            </li>
-        <?php } ?>
-    </ul>
-    
+    <?=SessionGraph::widget();?>
+
 </div>
