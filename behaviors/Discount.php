01f3d9c4f07cd1a703f30e65d0037aa3fb2ff6a0
Ilya <php@php.name>
2016-09-23 16:20:24 +0500

    service discount behavior

---
+++ behaviors/Discount.php
@@ -0,0 +1,35 @@
+<?php
+namespace pistol88\service\behaviors;
+
+use yii;
+use yii\base\Behavior;
+use pistol88\cart\Cart;
+
+class Discount extends Behavior
+{
+    public $eventName = 'element_cost';
+    
+    public function events()
+    {
+        $eventName = $this->eventName;
+
+        return [
+            $eventName => 'doDiscount'
+        ];
+    }
+
+    public function doDiscount($event)
+    {
+        if($event->element->model == 'pistol88\service\models\Price') {
+            if(yii::$app->promocode->has()) {
+                $persent = yii::$app->promocode->get()->promocode->discount;
+
+                if($persent > 0 && $persent <= 100 && $event->cost > 0) {
+                    $event->cost = $event->cost-($event->cost*$persent)/100;
+                }
+            }
+        }
+        
+        return $this;
+    }
+}
