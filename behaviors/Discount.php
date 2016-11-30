9cec9044be2928a2672b0e6c297ecc3601273e69
Ilya <php@php.name>
2016-11-30 10:40:17 +0500

    check for target models

--- behaviors/Discount.php
+++ behaviors/Discount.php
@@ -21,7 +21,7 @@ class Discount extends Behavior
     public function doDiscount($event)
     {
         if($event->element->model == 'pistol88\service\models\Price') {
-            if(yii::$app->promocode->has()) {
+            if(yii::$app->promocode->has() && !yii::$app->promocode->targetModels) {
                 $persent = yii::$app->promocode->get()->promocode->discount;
 
                 if($persent > 0 && $persent <= 100 && $event->cost > 0) {
